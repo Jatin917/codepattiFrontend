e4eda5aae07d0dbe39f1e6f40abbb809f5fbfc28
Jatin917 <jkchandel02@gmail.com>
2025-04-04 00:34:18 +0530

    added sample data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
 import './App.css'
-
+import data from './Store/sample.json'
 function App() {
-
+  console.log(data);
   return (
     <div className='bg-red-500 text-xl font-semibold text-center'>CodePatt Frontend</div>
   )
