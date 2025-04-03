17b29f179336c6fe627e0dc6b381b22187b950bf
Jatin917 <jkchandel02@gmail.com>
2025-04-03 23:54:14 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className='bg-red-500 text-xl font-semibold text-center'>CodePatti Frontend</div>
+    <div className='bg-red-500 text-xl font-semibold text-center'>CodePatt Frontend</div>
   )
 }
 
